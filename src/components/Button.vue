9ef5b6c88b114b939bf2876671f5cf01237c2ee9
vm <vm@localhost.localdomain>
2021-04-11 02:39:26 -0400

    initial

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    
+    <button @click="helloBackFunc()" :style="{background: color}" >{{text}} </button>
+
+</template>
+<script>
+export default {
+    name : 'Button',
+    props:{
+        text:String,
+        color:String,
+    },
+    data(){
+        return {
+            helloBackVar: 'Hello Parent'
+        }
+    },
+
+    methods:{
+        helloBackFunc(){
+            this.$emit('toggle-add-task')
+        }
+    }
+
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+    <Button @toggle-add-task="$emit('toggle-add-task')" 
+    :color="showAddTask ? 'red' : 'green'" 
+    :text="showAddTask ? 'Close' : 'add task'" />
+</template>
+<script>
+
+import Button from './Button'
+
+
+
+export default {
+    name:'Header',
+    props:  {
+      title : {
+          type: String,
+          default: "MAXIMILIANO",
+          
+      },
+      showAddTask: Boolean,
+    },
+    components:{
+        Button
+    },
+    methods:{
+        thanks(m){
+            console.log(m)
+
+        }
+    },
+    emits:['toggle-add-task']
+
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+</style>
